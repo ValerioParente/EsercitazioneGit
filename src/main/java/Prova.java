ffbbfccd8658975ea6a82dbae63339dcd437dd1b
parva <valerio.parente2002@gmail.com>
2022-03-07 11:35:57 +0100

    primo commit

---
+++ src/main/java/Prova.java
@@ -0,0 +1,13 @@
+public class Prova {
+
+    int x = 10;
+    int y = 5;
+
+    public static void main(String[] args) {
+        Prova test = new Prova();
+
+        System.out.println(test.x + test.y);
+
+
+    }
+}
